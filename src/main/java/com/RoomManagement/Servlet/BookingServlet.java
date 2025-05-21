bd4d8386f5ffde46df0cf1d39db1a99f7203bd6f
IT24102116 <IT24102116@my.sliit.lk>
2025-05-21 15:41:03 +0530

    IT24102116 bug fixed

--- src/main/java/com/RoomManagement/Servlet/BookingServlet.java
+++ src/main/java/com/RoomManagement/Servlet/BookingServlet.java
@@ -8,8 +8,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-import javax.servlet.*;
-import javax.servlet.http.*;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
